7f053932910e42b89f581c2d8bdfb7d2f8b61c7d
unknown <DELL@DESKTOP-R60B2U1>
2023-06-30 17:16:22 -0400

    Interpreter

---
+++ Interpreter_Basic_Program/Interpreter_Basic_Program/Program.vb
@@ -0,0 +1,29 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+		Dim n As Integer
+		Dim s As Integer
+		Dim a As Integer
+		Dim b As Integer
+		Dim x As Integer
+		Dim p As Integer
+		n = 2
+		s = 0
+		s = s + n
+		Console.WriteLine(n)
+		Console.WriteLine(s)
+		a = 5
+		b = a
+		b = b / 2
+		b = b * 2
+		b = a - b
+		Console.WriteLine(b)
+		x = 0
+		p = 0
+		x = b * p
+		x = x + x
+		p = p * 10
+		Console.WriteLine(p)
+	End Sub
+End Module
